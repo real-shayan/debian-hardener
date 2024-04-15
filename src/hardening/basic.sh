3ff19d781eb5503f1b86169cca0339a0386643cf
root <root@DESKTOP-G9N49VF>
2024-04-15 16:50:46 +0330

    basic: bugfix

--- src/hardening/basic.sh
+++ src/hardening/basic.sh
@@ -171,6 +171,6 @@ audit() {
     cat "files/auditd/record_login_logout" >>$AUDITPATH/record_login_logout.rules
     cat "files/auditd/record_mac_controls" >>$AUDITPATH/record_mac_controls.rules
     cat "files/auditd/record_networking" >>$AUDITPATH/record_networking.rules
-    cat "files/record_user_group" >>$AUDITPATH/record_user_group.rules
+    cat "files/auditd/record_user_group" >>$AUDITPATH/record_user_group.rules
     echo "Done!"
 }
